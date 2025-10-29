4decd77b78dd203160d9e2e5d0ab2eba74c3f58d
Nighdel <113827014+Jerry1921@users.noreply.github.com>
2025-10-29 23:46:22 +0600

    HQ9+

---
+++ prob_133A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class prob_133A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String inputString = sc.next();
+
+        //System.out.println(inputString);
+
+        if (inputString.contains("H") || inputString.contains("Q") || inputString.contains("9")){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+
+        //char[] charArr = inputString.toCharArray();
+
+        //if (charArr.)
+    }
+}
