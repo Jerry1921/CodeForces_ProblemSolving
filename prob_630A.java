a2b6c74cd97372b386b65dc5591f8a9f9a33227b
Jeriomio <113827014+Jerry1921@users.noreply.github.com>
2025-11-24 00:21:43 +0600

    Add files via upload

---
+++ prob_630A.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class prob_630A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        //int n = sc.nextInt();
+
+        //int powValue = (int)Math.pow(5,n);
+
+        //int firstValue = powValue % 100;
+
+       // String myStr = String.valueOf(powValue);
+
+        //System.out.println(myStr.substring(myStr.length()-2));
+
+        //System.out.println(String.valueOf(powValue).substring(2));
+
+        /*int secondValue = powValue % 10;
+        System.out.println(secondValue);*/
+
+
+        //System.out.println(firstValue+""+secondValue);
+
+
+        //System.out.println(powValue);
+
+
+
+        long n = sc.nextLong();
+
+        if (n == 0){
+            System.out.println(1);
+        }else {
+            System.out.println(25);
+        }
+    }
+}
