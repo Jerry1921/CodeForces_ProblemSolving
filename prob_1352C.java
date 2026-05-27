17d3c78e85ae8877a9231de009749b302fa34553
unknown <jerryjeriomio19@gmail.com>
2026-05-27 09:11:48 +0600

    Kth not divisible by n (one solution)

---
+++ prob_1352C.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class prob_1352C {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0){
+
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            long ans = k + (k-1)/(n-1);
+
+            System.out.println(ans);
+        }
+    }
+}
